1efe5767d8f811f0b3c72d572957a06a774a12a1
fernando daniel callejas robles <ferkyjaz@gmail.com>
2020-09-12 19:27:57 -0500

    Fixed submodule react js problem

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Joke from './Joke';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <h3>Welcome to pychain</h3>
+    </div>
+  );
+}
+
+export default App;
